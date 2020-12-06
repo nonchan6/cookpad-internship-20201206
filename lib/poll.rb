c8ebb51f6b8e72343c6da1af1a3799bb59256e1a
Granger Hermione <grangerhermione@N.local>
2020-12-06 16:04:17 +0900

    tmp2

--- lib/poll.rb
+++ lib/poll.rb
@@ -1,8 +1,35 @@
 class Poll
-  attr_reader :title, :candidates
+  class InvalidCandidateError < StandardError
+
+  end
+
+  attr_reader :title, :candidates, :votes
 
   def initialize(title, candidates)
     @title = title
     @candidates = candidates
+    @votes = []
+  end
+
+  def add_vote(vote)
+    unless candidates.include?(vote.candidate)
+      raise InvalidCandidateError
+    end
+
+    @votes.push(vote)
+  end
+
+  def count_votes
+    result = {}
+
+    candidates.each do |candidate|
+      result[candidate] = 0
+    end
+
+    votes.each do |vote|
+      result[vote.candidate] += 1
+    end
+
+    result.sort_by { |_key, value| value }.reverse.to_h
   end
 end
--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/poll'
+require_relative '../lib/vote'
 
 RSpec.describe Poll do
   it 'has a title and candidates' do
@@ -6,5 +7,50 @@ RSpec.describe Poll do
 
     expect(poll.title).to eq 'Awesome Poll'
     expect(poll.candidates).to eq ['Alice', 'Bob']
+  end
+
+  describe '#add_vote' do
+    it 'saves the given vote' do
+      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+      vote = Vote.new('Miyoshi', 'Alice')
+
+      poll.add_vote(vote)
+
+      expect(poll.votes).to eq [vote]
+    end
+
+    context 'with a vote that has an invalid candidate' do
+      it 'raises InvalidCandidatError' do
+        poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+        vote = Vote.new('Miyoshi', 'INVALID')
+
+      expect { poll.add_vote(vote) }.to raise_error Poll::InvalidCandidateError
+    end
+   end
+  end
+
+  describe '#count_votes' do
+    it 'counts the votes and sorts the candidates by the number of votes' do
+      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+      poll.add_vote(Vote.new('Carol', 'Alice'))
+      poll.add_vote(Vote.new('Dave', 'Alice'))
+      poll.add_vote(Vote.new('Ellen', 'Bob'))
+
+      result = poll.count_votes
+
+      expect(result['Alice']).to eq 2
+      expect(result['Bob']).to eq 1
+      expect(result.keys).to eq ['Alice', 'Bob']
+
+      poll2 = Poll.new('Great Poll', ['Alice', 'Bob'])
+      poll2.add_vote(Vote.new('Carol', 'Bob'))
+      poll2.add_vote(Vote.new('Dave', 'Bob'))
+
+      result2 = poll2.count_votes
+
+      expect(result2['Alice']).to eq 0
+      expect(result2['Bob']).to eq 2
+      expect(result2.keys).to eq ['Bob', 'Alice']
+    end
   end
 end
